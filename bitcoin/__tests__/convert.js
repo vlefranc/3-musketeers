e14b25a3dee5d4b3a1dfdd95c31baa7cecc09e69
Yassine AZZOUT <yassine@synthesio.com>
2018-03-09 14:57:25 +0100

    fix(bitcoin): describe tests with more indication

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -3,12 +3,12 @@
 const convert = require('..');
 const Big = require('big.js');
 
-test('should default to returning a Number', () => {
+test('should default to returning then default ifself', () => {
   //convert(2, 'BTC', 'BTC');
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should return a Number', () => {
+test('should return a number', () => {
   //convert(2, 'BTC', 'BTC', 'Number');
   throw new Error('test not yet defined... write your test here');
 });
@@ -18,17 +18,17 @@ test('should return a Big number', () => {
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should return a String', () => {
+test('should return a string', () => {
   //convert(2100, 'mBTC', 'BTC', 'String');
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should convert an integer', () => {
+test('should convert a number from interger', () => {
   //convert(123456789012345, 'Satoshi', 'BTC', 'Number');
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should convert a number', () => {
+test('should convert a number from float', () => {
   //convert(1234567.89012345, 'BTC', 'Satoshi', 'Number');
   throw new Error('test not yet defined... write your test here');
 });
@@ -43,13 +43,13 @@ test('should convert a Big number', () => {
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should convert a NaN to a Number', () => {
+test('should convert a NaN to a number', () => {
   //convert(NaN, 'BTC', 'BTC', 'Number');
   //convert(NaN, 'BTC', 'mBTC', 'Number');
   throw new Error('test not yet defined... write your test here');
 });
 
-test('should convert a NaN to a String', () => {
+test('should convert a NaN to a string', () => {
   //convert(NaN, 'BTC', 'BTC', 'String');
   //convert(NaN, 'BTC', 'mBTC', 'String');
   throw new Error('test not yet defined... write your test here');
